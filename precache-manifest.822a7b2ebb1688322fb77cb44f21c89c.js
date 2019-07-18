0ac13f6e60c3574e7ac92dd4af8a08922202fb3b
Dominic Albert Santos <dominicalbertsantos@Dominic-Albert-MacBook-Pro.local>
2019-07-18 20:37:19 +0900

    Updates

---
+++ precache-manifest.822a7b2ebb1688322fb77cb44f21c89c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f01580c652ddbb86e7291fe558896b5e",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2ec6ed01037e4890ba93",
+    "url": "/monster-rolodex/static/css/main.081bc48b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2ec6ed01037e4890ba93",
+    "url": "/monster-rolodex/static/js/main.d6552889.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
